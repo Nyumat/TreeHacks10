229f69f75ebb150d15ccf6afe311deeae1348a2d
TanzHasan <tanzirhasanhasan@gmail.com>
2024-02-17 00:55:17 -0800

    added new endpoint with OCR

--- minerva/main.py
+++ minerva/main.py
@@ -1,13 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+import pytesseract
+from PIL import Image
+import numpy as np
+import io
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
 
-
 @app.get("/items/{item_id}")
 def read_item(item_id: int, query_param: str = None):
     return {"item_id": item_id, "query_param": query_param}
+
+@app.post("/upload-image/")
+async def create_upload_file(file: UploadFile = File(...)):
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents))
+    
+    text = pytesseract.image_to_string(image)
+    
+    return JSONResponse(content={"text": text})
