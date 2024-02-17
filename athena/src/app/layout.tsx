caab6b674c3392afa4a65dcd7ca64a078a9b52ed
Thomas <nyumat18@gmail.com>
2024-02-17 05:50:14 -0800

    design system: added shadcn

--- athena/src/app/layout.tsx
+++ athena/src/app/layout.tsx
@@ -1,9 +1,13 @@
 import ConvexClientProvider from "@/(components)/ConvexClientProvider";
+import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Athena",
@@ -17,7 +21,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
         <ConvexClientProvider>{children}</ConvexClientProvider>
       </body>
     </html>
